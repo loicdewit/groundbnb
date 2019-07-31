3736514e37219a843fab6ec0790f15d8f16cc9ec
Khurram Mohammed <khurrammohammed1990@gmail.com>
2019-07-31 11:23:11 +0100

    groundBNB skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+resources :users do
+  resources :listings
+  end
+
+  root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20190731100908_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.NVARCHAR :email
+      t.VARCHAR :password
+
+      t.timestamps
+    end
+  end
+end
