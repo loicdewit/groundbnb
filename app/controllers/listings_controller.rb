5bac8982b435ad55d27a8fa8b1cfa3e9bfa01b6d
Loïc de Wit <dew.loic@gmail.com>
2019-08-01 16:49:52 +0100

    resolved changes with embedded resource

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,17 @@
 class ListingsController < ApplicationController
 
   def new
-    
   end
 
+  def create
+    @user = User.find(params[:user_id])
+    @listing = @user.listings.create(listing_params)
+    @listing.save
+    p @listing
+    redirect_to ('/')
+  end
+
+  private def listing_params
+    params.require(:listing).permit(:description)
+  end
 end
